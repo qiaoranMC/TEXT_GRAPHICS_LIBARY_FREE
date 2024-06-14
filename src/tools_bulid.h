7828e48d766607a4e130891f387919769dc35055
qiaoranMC <113179113+qiaoranMC@users.noreply.github.com>
2024-06-14 15:48:40 +0800

    Add files via upload

---
+++ src/tools_bulid.h
@@ -0,0 +1,22 @@
+char a2hole1(char dcl[0xfffff]){
+	char db[0xfff]={0},*out,clh[0xfff][0xff];
+	int local[10];/*
+	clh[0][0]=10;local[0]=0;local[1]=1;
+	for(local[0]=0;local[0]<10000;){
+		if(dcl[local[0]]==10){
+			db[local[1]]=local[2];local[1]++;local[2]=0;
+		}else if(dcl[local[0]]==-1){
+			clh[local[1]][local[2]]=10;
+			break;
+		}
+		clh[local[1]][local[2]]=dcl[local[0]];local[2]++;local[0]++;
+	}
+	local[0]=0;local[1]=0;local[2]=0;
+	for(local[0]=0;local[0]<0xfff;local[0]++){
+		for(local[1]=db[local[0]];local[1]>=0;local[1]--){
+	    		out[local[2]]=clh[local[0]][local[1]];local[2]++;
+		}
+		out[local[2]]=10;local[2]++;
+	}*/
+	return out;
+}
