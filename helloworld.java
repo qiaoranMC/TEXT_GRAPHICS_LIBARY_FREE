3bc988886765640cfc56ea1f0566f3089002283c
qiaoranMC <113179113+qiaoranMC@users.noreply.github.com>
2024-07-08 16:44:01 +0800

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
-public class helloworld(){
-     public static Main(String, args[]){
+public class helloworld{
+     public static void Main(String, args[]){
           System.out.println("Hello world!\n");
      }
 }
